5ca96e122139e4ad083b44a8be21f5cb3cf9ce06
Anuj Dubey <anujd2408@gmail.com>
2025-01-16 01:25:07 +0530

    added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,42 +1,21 @@
-"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-
-const Login = () => {
-  const { status } = useSession();
-  const router = useRouter();
-
-  // Use useEffect to handle navigation
-  useEffect(() => {
-    if (status === "authenticated") {
-      router.push("/");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
+const LoginPage = () => {
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
-      {/* Box */}
-      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
-        {/* Image */}
+      {/* BOX */}
+      <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        {/* IMAGE CONTAINER */}
         <div className="relative h-1/3 w-full md:h-full md:w-1/2">
-          <Image src="/LoginPage.jpg" alt="" fill className="object-cover" />
+          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
         </div>
-        {/* Form */}
+        {/* FORM CONTAINER */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
-          <p>Log into your account or create a new one!</p>
-          <button
-            className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md"
-            onClick={() => signIn("google")}
-          >
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
             <Image
               src="/google.png"
               alt=""
@@ -48,7 +27,7 @@ const Login = () => {
           </button>
           <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
             <Image
-              src="/fb.png"
+              src="/facebook.png"
               alt=""
               width={20}
               height={20}
@@ -57,10 +36,7 @@ const Login = () => {
             <span>Sign in with Facebook</span>
           </button>
           <p className="text-sm">
-            Have a problem?{" "}
-            <Link className="underline" href="/">
-              Contact us
-            </Link>
+            Have a problem?<Link className="underline" href="/"> Contact us</Link>
           </p>
         </div>
       </div>
@@ -68,4 +44,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default LoginPage;
