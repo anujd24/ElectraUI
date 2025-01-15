783c175a1c1bffac34aa2934253acaa0edc19f84
Anuj Dubey <anujd2408@gmail.com>
2025-01-16 01:23:46 +0530

    added

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,75 +1,47 @@
-"use client"
-
-import React, { useEffect } from 'react'
-import Image from 'next/image'
-import { useCartStore } from "@/utils/store";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import React from "react";
 
 const CartPage = () => {
-
-  const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    useCartStore.persist.rehydrate();
-  }, []);
-
-  const handleCheckout = async () => {
-    if (!session) {
-      router.push("/login");
-    } else {
-      try {
-        const res = await fetch("http://localhost:3000/api/orders", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            price: totalPrice,
-            products,
-            status: "Not Paid!",
-            userEmail: session.user.email,
-          }),
-        });
-        const data =await res.json()
-        router.push(`/pay/${data.id}`)
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
-
   return (
-    <div className='h-calc[(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-gray-600 lg:flex-row'>
-      {/* Products */}
-      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
-      {/* Single Product */}
-      {products.map((item) => (
-          <div className="flex items-center justify-between mb-4" key={item.id}>
-            {item.img && (
-              <Image src={item.img} alt="" width={100} height={100} />
-            )}
-            <div className="">
-              <h1 className="uppercase text-xl font-bold">
-                {item.title} x{item.quantity}
-              </h1>
-              <span>{item.optionTitle}</span>
-            </div>
-            <h2 className="font-bold">${item.price}</h2>
-            <span
-              className="cursor-pointer"
-              onClick={() => removeFromCart(item)}
-            >
-              X
-            </span>
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-gray-600 lg:flex-row">
+      {/* PRODUCTS CONTAINER */}
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+        {/* SINGLE ITEM */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/dellInsp.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">dell inspiron</h1>
+            <span>Large</span>
           </div>
-        ))}
+          <h2 className="font-bold">₹40,000</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE ITEM */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/dellInsp.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">dell inspiron</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">₹40,000</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+        {/* SINGLE ITEM */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/dellInsp.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-xl font-bold">dell inspiron</h1>
+            <span>Large</span>
+          </div>
+          <h2 className="font-bold">₹40,000</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
       </div>
-    {/* payment */}
-    <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+      {/* PAYMENT CONTAINER */}
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
-          <span className="">Subtotal ({totalItems} items)</span>
-          <span className="">₹{totalPrice}</span>
+          <span className="">Subtotal (3 items)</span>
+          <span className="">₹120000</span>
         </div>
         <div className="flex justify-between">
           <span className="">Service Cost</span>
@@ -82,12 +54,9 @@ const CartPage = () => {
         <hr className="my-2" />
         <div className="flex justify-between">
           <span className="">TOTAL(INCL. VAT)</span>
-          <span className="font-bold">₹{totalPrice}</span>
+          <span className="font-bold">₹81.70</span>
         </div>
-        <button
-          className="bg-gray-500 text-white p-3 rounded-md w-1/2 self-end"
-          onClick={handleCheckout}
-        >
+        <button className="bg-gray-500 text-white p-3 rounded-md w-1/2 self-end">
           CHECKOUT
         </button>
       </div>
@@ -95,4 +64,4 @@ const CartPage = () => {
   );
 };
 
-export default CartPage
+export default CartPage;
