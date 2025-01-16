0e376849992941270a133ef3f6fcef50fe8e36ac
Anuj Dubey <anujd2408@gmail.com>
2025-01-16 19:15:23 +0530

    added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import Menu from './menu'
-import Link from 'next/link'
-import Image from 'next/image'
-import CartIcon from './cartIcon'
+import React from "react";
+import Menu from "./menu";
+import Link from "next/link";
+import CartIcon from "./cartIcon";
+import Image from "next/image";
+
 const Navbar = () => {
-
-    const user = false;
-
+  const user = false;
   return (
-    <div className='h-12 text-gray-600 p-4 flex items-center justify-between border-b-2 border-b-gray-600 uppercase md:h-24
-    lg:px-20 xl:px-40'>
-        <div className='hidden md:flex gap-4 flex-1'>
-            <Link href="/">Homepage</Link>
-            <Link href="/menu">Menu</Link>
-            <Link href="/">Contact</Link>
+    <div className="h-12 text-gray-500 p-4 flex items-center justify-between border-b-2 border-b-gray-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Electra</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>123 456 78</span>
         </div>
-
-        <div className='text-gray-600 font-bold text-xl md:font-bold flex-1 md:text-center'>
-            <Link href="/">
-            Electra
-            </Link>
-        </div>
-        {/* Mobile Menu */}
-        <div className='md:hidden'>
-            <Menu/>
-        </div>
-
-    {/* Right Links */}
-        <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
-            <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-gray-600 px-1 rounded-md text-white'>
-                <Image src="/phone.png" alt='' width={20} height={20}></Image>
-                <span>999888777</span>    
-            </div>
-            <CartIcon/>
-        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
     </div>
   );
 };
