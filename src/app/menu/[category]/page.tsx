d9a205859e2e17059281a465283033cb13a3c86e
Anuj Dubey <anujd2408@gmail.com>
2025-02-03 22:35:06 +0530

    Added

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -11,8 +11,11 @@ const categoryData: Record<string, typeof laptops | typeof Phones | typeof TV> =
   tv: TV,
 };
 
+interface CategoryPageProps {
+  params: Awaited<{ category: string }>;
+}
 
-const CategoryPage = async ({ params }: { params: { category: string } }) => {
+const CategoryPage = async ({ params }: CategoryPageProps) => {
   const { category } = params;
   const products = categoryData[category?.toLowerCase()] || [];
 
@@ -25,11 +28,13 @@ const CategoryPage = async ({ params }: { params: { category: string } }) => {
             href={`/product/${item.id}`}
             key={item.id}
           >
+            {/* IMAGE CONTAINER */}
             {item.img && (
               <div className="relative h-[80%]">
                 <Image src={item.img} alt={item.title} fill className="object-contain" />
               </div>
             )}
+            {/* TEXT CONTAINER */}
             <div className="flex items-center justify-between font-bold">
               <h1 className="text-2xl uppercase p-2">{item.title}</h1>
               <h2 className="group-hover:hidden text-xl">₹{item.price}</h2>
